dfb70d86f55870d41611321c03ed30c0080649c5
xuzewei <xuzewei_2012@126.com>
2017-04-10 14:45:13 +0800

    Description.java

---
+++ src/main/java/com/github/xuzw/api_engine_runtime/annotation/Description.java
@@ -0,0 +1,26 @@
+package com.github.xuzw.api_engine_runtime.annotation;
+
+import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
+import static java.lang.annotation.ElementType.CONSTRUCTOR;
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.LOCAL_VARIABLE;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.PACKAGE;
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.ElementType.TYPE_PARAMETER;
+import static java.lang.annotation.ElementType.TYPE_USE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+@Retention(RUNTIME)
+@Target({ TYPE, FIELD, METHOD, PARAMETER, CONSTRUCTOR, LOCAL_VARIABLE, ANNOTATION_TYPE, PACKAGE, TYPE_PARAMETER, TYPE_USE })
+/**
+ * @author 徐泽威 xuzewei_2012@126.com
+ * @time 2017年4月10日 下午2:15:51
+ */
+public @interface Description {
+    String value();
+}
